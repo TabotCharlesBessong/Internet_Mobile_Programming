1f143e12726931c924032fd41c54b38b28d1b7f7
Nerus-Zaumu <neruszaumuy@gmail.com>
2023-06-18 17:26:17 +0100

    fix: started working on map

--- digitekisi-mobile/src/app/dashboard/home/home.component.ts
+++ digitekisi-mobile/src/app/dashboard/home/home.component.ts
@@ -1,37 +1,44 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { GoogleMap } from '@capacitor/google-maps';
 import { environment } from 'src/environments/environment';
+import { CommonModule } from '@angular/common';
+import { Geolocation } from '@capacitor/geolocation'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule],
+  imports: [IonicModule, FormsModule, CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent  implements OnInit, AfterViewInit {
 
-  @ViewChild('map') map!: ElementRef
+  @ViewChild('mapRef') mapRef!: ElementRef<HTMLElement>
+  newMap!: GoogleMap
   activeStatus = 'active'
 
   constructor() { }
 
   ngOnInit() {
-    this.initMap()
+  }
+
+  ngAfterViewInit(): void {
+     this.initMap()
   }
 
   async initMap(){
-    const newMap = await GoogleMap.create({
-      id: 'map',
-      element: this.map.nativeElement,
+    const { coords } = await Geolocation.getCurrentPosition()
+    this.newMap = await GoogleMap.create({
+      id: 'digitekisi-map',
+      element: this.mapRef.nativeElement,
       apiKey: environment.GOOGLE_MAP_API_KEY,
       config: {
         center: {
-          lat: 33.6,
-          lng: -117.9,
+          lat: coords.latitude,
+          lng: coords.longitude,
         },
         zoom: 8,
       },
